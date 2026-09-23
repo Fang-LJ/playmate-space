1ea0d08e14b7cd4a93e7e8d2d3692a9cb5ef2361
fangliangjun <fangliangjun@ysstech.com>
2026-09-23 09:18:34 +0800

    fix: avoid randomized certificate renewal delay

--- deploy/renew-cert.sh
+++ deploy/renew-cert.sh
@@ -6,7 +6,7 @@ cd "$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 docker run --rm \
   -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
   -v "$(pwd)/certbot/www:/var/www/certbot" \
-  certbot/certbot:latest renew --webroot -w /var/www/certbot
+  certbot/certbot:latest renew --no-random-sleep-on-renew --webroot -w /var/www/certbot
 
 docker compose --env-file .env.prod -f docker-compose.prod.yml \
   exec -T playmate-nginx nginx -s reload
